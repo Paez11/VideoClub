38bdd3ddcba4cd3e98ef5d741b74034af253390a
Gilarte <rafagilarte@gmail.com>
2022-03-03 11:15:38 +0100

    utils acabado, falta vistas

---
+++ src/main/java/vistas/vistas.java
@@ -0,0 +1,37 @@
+package vistas;
+
+import interfaces.IVista;
+
+public class vistas implements IVista {
+
+	
+	public void showMenuPrincipal() {
+		System.out.println("[̲̅V̲̅][̲̅i̲̅][̲̅d̲̅][̲̅e̲̅][̲̅o̲̅][̲̅c̲̅][̲̅l̲̅][̲̅u̲̅][̲̅b̲̅]");
+		/**System.out.println("____    ____  __   _______   _______   ______     ______  __       __    __  .______\r\n"
+				+ "\\   \\  /   / |  | |       \\ |   ____| /  __  \\   /      ||  |     |  |  |  | |   _  \\\r\n"
+				+ " \\   \\/   /  |  | |  .--.  ||  |__   |  |  |  | |  ,----'|  |     |  |  |  | |  |_)  |\r\n"
+				+ "  \\      /   |  | |  |  |  ||   __|  |  |  |  | |  |     |  |     |  |  |  | |   _  <\r\n"
+				+ "   \\    /    |  | |  '--'  ||  |____ |  `--'  | |  `----.|  `----.|  `--'  | |  |_)  |\r\n"
+				+ "    \\__/     |__| |_______/ |_______| \\______/   \\______||_______| \\______/  |______/");**/
+		System.out.println("");
+		System.out.println("1. Catálogo de productos.");
+		System.out.println("2. Catálogo de clientes.");
+		System.out.println("3. Gestión de reserva.");
+		System.out.println("0. Salir.");
+	}
+
+	public void showMenuProductos() {
+		
+	}
+
+	public void showMenuClientes() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void showMenuReservas() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
